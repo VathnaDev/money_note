0618eca9f692ef6d9eea266759bf1125e761b165
Luther <vathnadev@gmail.com>
2022-03-14 14:23:57 +0700

    feat(currency): adjust style

--- lib/src/screens/currency/curreny_screen.dart
+++ lib/src/screens/currency/curreny_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:money_note/src/data/currency.dart';
+import 'package:money_note/src/utils/theme.dart';
 
 class CurrencyScreen extends HookConsumerWidget {
   CurrencyScreen({
@@ -25,12 +26,29 @@ class CurrencyScreen extends HookConsumerWidget {
       appBar: AppBar(
         title: Text("Currency"),
       ),
-      body: ListView(
-        children: [
-          ...currencies.map(
-            (e) => RadioListTile<Currency>(
+      body: ListView.separated(
+          padding: const EdgeInsets.symmetric(vertical: 16),
+          itemCount: currencies.length,
+          separatorBuilder: (BuildContext context, int index) => const Divider(
+                indent: 30,
+                endIndent: 20,
+                thickness: 1,
+                height: 1,
+              ),
+          itemBuilder: (BuildContext context, int index) {
+            final e = currencies[index];
+            return RadioListTile<Currency>(
+              contentPadding:
+                  const EdgeInsets.symmetric(vertical: 2, horizontal: 16),
               title: Row(
                 children: [
+                  Container(
+                    width: 35,
+                    height: 35,
+                    decoration: boxDecoration,
+                    child: Center(child: Text(e.symbol)),
+                  ),
+                  SizedBox(width: 8),
                   Expanded(child: Text("${e.symbol} 100.000")),
                   Text(e.abbreviate)
                 ],
@@ -40,10 +58,8 @@ class CurrencyScreen extends HookConsumerWidget {
               },
               groupValue: currency.value,
               value: e,
-            ),
-          )
-        ],
-      ),
+            );
+          }),
     );
   }
 }
