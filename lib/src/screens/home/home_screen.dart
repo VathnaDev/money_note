8da7295b660f157f55e78fec5204879c399c4ddb
Luther Vuthy <luther.vuthy@pathmazing.com>
2022-03-24 16:07:09 +0700

    feat(home): improve animation

--- lib/src/screens/home/home_screen.dart
+++ lib/src/screens/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -26,7 +27,13 @@ class HomeScreen extends HookConsumerWidget {
 
     return Scaffold(
       body: AnimatedSwitcher(
-        duration: const Duration(milliseconds: 500),
+        transitionBuilder: (child, animation) {
+          return FadeTransition(
+            opacity: animation,
+            child: child,
+          );
+        },
+        duration: const Duration(milliseconds: 250),
         child: screens[selectedTab.value],
       ),
       bottomNavigationBar: BottomNavigationBar(
